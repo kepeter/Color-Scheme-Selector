662a2de2e010cbb9aa9e47a9433b4641316e03ef
Kornfeld Eliyahu Peter <kepeter@gmail.com>
2015-03-15 20:34:22 +0200

    Original

---
+++ ColorSchemeSelector/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.ColorSchemeSelector
+{
+    static class GuidList
+    {
+        public const string guidColorSchemeSelectorPkgString = "21ba397b-c8ca-4b78-862f-47c457493c46";
+        public const string guidColorSchemeSelectorCmdSetString = "edcf5d10-812c-4739-8471-ef23df1f603a";
+        public const string guidToolWindowPersistanceString = "2b119b11-7f31-4e10-adf1-6b43a180dbac";
+
+        public static readonly Guid guidColorSchemeSelectorCmdSet = new Guid(guidColorSchemeSelectorCmdSetString);
+    };
+}
---
+++ ColorSchemeSelector/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Company.ColorSchemeSelector
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdidColorSchemeSelector =    0x101;
+
+    };
+}
