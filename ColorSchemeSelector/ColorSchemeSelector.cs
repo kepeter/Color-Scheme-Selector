6968930b8b8423ebf6df78174fc05de5aaebad6a
Kornfeld Eliyahu Peter <kepeter@gmail.com>
2015-04-26 23:24:14 +0300

    Add missing files

---
+++ ColorSchemeSelector/ColorSchemeSelector.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ColorSchemeSelector
+{
+    [PackageRegistration ( UseManagedResourcesOnly = true )]
+    [InstalledProductRegistration ( "#110", "#112", "1.0", IconResourceID = 400 )]
+    [ProvideMenuResource ( "Menus.ctmenu", 1 )]
+    [ProvideToolWindow ( typeof ( ColorSchemeToolWindow ), Style = Microsoft.VisualStudio.Shell.VsDockStyle.Tabbed )]
+    [Guid ( Guids.Package )]
+    public sealed class ColorSchemeSelector : Package
+    {
+        public ColorSchemeSelector ( )
+        {
+        }
+
+        private void ShowToolWindow ( object sender, EventArgs e )
+        {
+            ToolWindowPane window = this.FindToolWindow ( typeof ( ColorSchemeToolWindow ), 0, true );
+            if ( ( null == window ) || ( null == window.Frame ) )
+            {
+                throw new NotSupportedException ( "Can not create tool window." );
+            }
+            IVsWindowFrame windowFrame = ( IVsWindowFrame )window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure ( windowFrame.Show ( ) );
+        }
+
+        #region Package Members
+
+        protected override void Initialize ( )
+        {
+            base.Initialize ( );
+
+            OleMenuCommandService oOleMenuCommandService = ( OleMenuCommandService )GetService ( typeof ( IMenuCommandService ) );
+
+            if ( oOleMenuCommandService != null )
+            {
+                CommandID oCommandID = new CommandID ( new Guid ( Guids.CommandSet ), ( int )CommandIDs.ColorSchemeSelector );
+                MenuCommand oMenuCommand = new MenuCommand ( ShowToolWindow, oCommandID );
+
+                oOleMenuCommandService.AddCommand ( oMenuCommand );
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ ColorSchemeSelector/Consts.cs
@@ -0,0 +1,16 @@
+﻿namespace ColorSchemeSelector
+{
+    static class Guids
+    {
+        public const string Package = "21ba397b-c8ca-4b78-862f-47c457493c46";
+        public const string CommandSet = "edcf5d10-812c-4739-8471-ef23df1f603a";
+        //public const string ToolWindow = "2b119b11-7f31-4e10-adf1-6b43a180dbac";
+    };
+
+    static class CommandIDs
+    {
+
+        public const uint ColorSchemeSelector = 0x101;
+
+    };
+}
